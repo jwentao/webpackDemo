55a022766703df0233c3524f0f8d68c586ccadba
jinwt <380793594@qq.com>
2018-04-19 16:50:44 +0800

    add style-loader

---
+++ style-loader/readme.md
@@ -0,0 +1,6 @@
+## style-loader配置
+### options
+* insertAt(插入位置：top, bottom)
+* insertInfo (插入到某个dom)
+* singleton (是否只使用一个style标签)
+* transform (转化，浏览器环境下，插入页面前)
---
+++ style-loader/src/app.js
@@ -0,0 +1,17 @@
+import './css/base.css'
+import './css/common.css'
+
+
+// 使用useable
+// import base from './css/base.css'
+// import common from './css/common.css'
+
+// let flag = false;
+// setInterval(function () {
+// 	if (flag) {
+// 		base.unuse();
+// 	} else {
+// 		base.use()
+// 	}
+// 	flag = !flag;
+// }, 500);
